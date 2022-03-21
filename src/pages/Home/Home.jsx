e18c9cfa2499bef2fc816bf98b5179e39fb6f5e6
Oriana Abreu <oriabreu.v@gmail.com>
2022-03-21 19:21:10 +0100

    create home component

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hola Mundo</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit dolore
+        distinctio numquam labore dignissimos ipsam? Libero ipsam id tempore
+        modi rem eum ad aliquid, quo tenetur necessitatibus dolorum ea eveniet
+      </p>
+    </>
+  );
+}
